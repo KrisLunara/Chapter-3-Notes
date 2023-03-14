9e775f3a8885e536fc71b5d2257ede43b3bb819c
9ut4 <9ut4@fresnou.org>
2023-03-14 14:19:08 -0700

    commit 03-14-2023 | Added more work to Functions1.js

--- Functions1.js
+++ Functions1.js
@@ -1,4 +1,14 @@
-//#1
-function findAreaOfRectangle(length, width){
+// //#1
+// function findAreaOfRectangle(length, width){
+//     console.log(`The area of a ${length} x ${width} rectangle is ${length*width}`);
+// }
+//
+// findAreaOfRectangle(10,10);
+// findAreaOfRectangle(3,8);
 
+//#2
+function findAreaOfCircle(radius){
+    console.log(`the area of a circle with a ${radius} radius is ${radius*3.14}`);
 }
+findAreaOfCircle(5)
+findAreaOfCircle(3)
