98878e6f869a650283c9ecccef60eee2801c3a8e
9ut4 <9ut4@fresnou.org>
2023-03-23 14:18:21 -0700

    commit 03-23-2023 | Added ArraysNotes.js & Arrays1.js , Updated Return1.js

---
+++ Arrays1.js
@@ -0,0 +1,16 @@
+//#1
+function firstElement(arr){
+    console.log(arr[0]);
+}
+let myNums = [1, 2, 3, 4];
+firstElement(myNums);
+
+//#2
+function lastElement(arr){
+    let lastArr = arr[arr.length - 1];
+    console.log(lastArr);
+}
+let Nums = [5, 6, 7, 8];
+lastElement(Nums);
+
+//#3
--- Return1.js
+++ Return1.js
@@ -13,12 +13,12 @@ const readLine = require('readline-sync');
 
 // //#2
 // function NumberGenerator(){
-//     let first = readLine.question("Enter a number: ");
-//     let last = readLine.question("Enter another: ");
+//     let first = Number(readLine.question("Enter a number: "));
+//     let last = Number(readLine.question("Enter another: "));
 //     if (first>=0 && first<=9 && last>=0 && last<=9){
 //         return(`${first}${last}`);
 //     } else {
-//         console.log("ERROR!\nInput numbers between 0-9");
+//         return ("ERROR!\nInput numbers between 0-9");
 //     }
 //     }
 // let total = NumberGenerator();
@@ -29,7 +29,7 @@ const readLine = require('readline-sync');
 // function Avg(temp1, temp2, temp3, temp4, temp5, temp6, temp7){
 //     let sum = temp1+temp2+temp3+temp4+temp5+temp6+temp7;
 //     let total= sum/7;
-//     console.log(Math.floor(total));
+//     return(Math.floor(total));
 // }
 //
 // Avg(50, 58, 53, 54, 61, 59, 60);
@@ -41,22 +41,22 @@ const readLine = require('readline-sync');
 //
 // }
 
-//#5
-function score(){
-    let score1 = readLine.question(`Enter round 1 score for player 1\n(0-180): `);
-    let score4 = readLine.question(`Enter round 1 score for player 2\n(0-180): `);
-    let score2 = readLine.question(`Enter round 2 score for player 1\n(0-180): `);
-    let score5 = readLine.question(`Enter round 2 score for player 2\n(0-180): `);
-    let score3 = readLine.question(`Enter round 3 score for player 1\n(0-180): `);
-    let score6 = readLine.question(`Enter round 3 score for player 2\n(0-180): `);
-    let player1 = score1 + score2 + score3;
-    let player2 = score4 + score5 + score6;
-    if(player1>player2){
-        console.log(`Player 1 wins!`);
-    } else if(player2>player1){
-        console.log(`Player 2 wins!`)
-    } else{
-        console.log("The game is tied!");
-    }
-}
-score();
+// //#5
+// function score(){
+//     let score1 = readLine.question(`Enter round 1 score for player 1\n(0-180): `);
+//     let score4 = readLine.question(`Enter round 1 score for player 2\n(0-180): `);
+//     let score2 = readLine.question(`Enter round 2 score for player 1\n(0-180): `);
+//     let score5 = readLine.question(`Enter round 2 score for player 2\n(0-180): `);
+//     let score3 = readLine.question(`Enter round 3 score for player 1\n(0-180): `);
+//     let score6 = readLine.question(`Enter round 3 score for player 2\n(0-180): `);
+//     let player1 = score1 + score2 + score3;
+//     let player2 = score4 + score5 + score6;
+//     if(player1>player2){
+//         console.log(`Player 1 wins!`);
+//     } else if(player2>player1){
+//         console.log(`Player 2 wins!`)
+//     } else{
+//         console.log("The game is tied!");
+//     }
+// }
+// score();
