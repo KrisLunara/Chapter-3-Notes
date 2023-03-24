844fcc6f6005efc45f8397a2f8af8c0bf7db5127
9ut4 <9ut4@fresnou.org>
2023-03-24 14:17:21 -0700

    commit 03-24-2023 | Added problems 3-6 to Arrays1.js

--- Arrays1.js
+++ Arrays1.js
@@ -1,6 +1,10 @@
 //#1
 function firstElement(arr){
-    console.log(arr[0]);
+    if (arr.length === 0){
+        console.log('Error\nNo items in array');
+    } else{
+        console.log(arr[0]);
+    }
 }
 let myNums = [1, 2, 3, 4];
 firstElement(myNums);
@@ -8,9 +12,36 @@ firstElement(myNums);
 //#2
 function lastElement(arr){
     let lastArr = arr[arr.length - 1];
-    console.log(lastArr);
+    if (arr.length === 0){
+        console.log('Error\nNo items in array');
+    }else {
+        console.log(lastArr);
+    }
 }
 let Nums = [5, 6, 7, 8];
 lastElement(Nums);
 
-//#3
+//#4
+function revElement(arr){
+    for(let rev=arr.length-1; rev>=0; rev--){
+        console.log(arr[rev]);
+    }
+}
+revElement(myNums);
+
+//#5
+function s3Element(arr){
+    for(let rev=arr.length-1; rev>=0; rev-=3){
+        console.log(arr[rev]);
+    }
+}
+let Skip = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+s3Element(Skip);
+
+//#6
+function s2Element(arr){
+    for(let rev=arr.length-2; rev>=0; rev-=2){
+        console.log(arr[rev]);
+    }
+}
+s2Element(Skip);
