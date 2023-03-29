faa70e66f21e9d42d2ec7bb83b5e1ffdecaa4594
9ut4 <9ut4@fresnou.org>
2023-03-29 14:19:57 -0700

    commit 03-29-2023 | Added problems 3, 5, 6 to Arrays2.js

--- Arrays2.js
+++ Arrays2.js
@@ -1,28 +1,63 @@
-//#1 & #2
-function count(n){
-    let myArr=[];
-    if (n<=0){
-        console.log('ERROR!')
-    } else {
-        for (let i=1; i<=n; i++){
-            myArr.push(i);
-        }
-        console.log(myArr);
-    }
-}
-count(-5);
-count(7);
-
-// //#3
-// function Xes(x){
+// //#1 & #2
+// function count(n){
 //     let myArr=[];
-//     if (x<=0){
+//     if (n<=0){
 //         console.log('ERROR!')
 //     } else {
-//         for (let i=1; i<=x; i++){
+//         for (let i=1; i<=n; i++){
 //             myArr.push(i);
 //         }
 //         console.log(myArr);
 //     }
 // }
-// Xes(2)
+// count(-5);
+// count(7);
+
+// //3
+// function ReplaceX(arr, x){
+//     for (let i=0; i<arr.length; i++){
+//         arr[i]=x;
+//     }
+//     console.log(arr)
+// }
+//
+// ReplaceX([1, 4, 1, 7], 4)
+
+// //#5
+// function BiggerX(arr){
+//     if (arr[0] > arr[arr.length-1]){
+//         for (let i=0; i<arr.length; i++){
+//             arr[i]=arr[0];
+//         }
+//         console.log(arr);
+//     } else if (arr[0] < arr[arr.length-1]){
+//         for (let i=0; i<arr.length; i++){
+//             arr[i]=arr[1];
+//         }
+//         console.log(arr);
+//     } else if (arr[0] === arr[arr.length-1]){
+//         console.log(arr);
+//     }
+// }
+// BiggerX([4, 1]);
+// BiggerX([5, 5]);
+// BiggerX([10, 10]);
+
+//#6
+function Compare(arr){
+    console.log(arr)
+    if (arr[0] > arr[arr.length-1]){
+        for (let i=0; i<arr.length; i++){
+            arr[i]=arr[0];
+        }
+        console.log(arr);
+    } else if (arr[0] < arr[arr.length-1]){
+        for (let i=0; i<arr.length; i++){
+            arr[i]=arr[1];
+        }
+        console.log(arr);
+    } else if (arr[0] === arr[arr.length-1]){
+        console.log(arr);
+    }
+}
+Compare([4, 8]);
